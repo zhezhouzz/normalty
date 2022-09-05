134905119aaa209e1bbfd8c85f5c71bfe1c54a58
zhouzhezz <zorichzorich@gmail.com>
2022-09-05 14:02:53 -0400

    add quantifier

--- src/ast.ml
+++ src/ast.ml
@@ -1,3 +1,18 @@
+module Q = struct
+  type t = Fa | Ex
+
+  let is_forall = function Fa -> true | Ex -> false
+  let is_exists x = not @@ is_forall x
+
+  let of_string = function
+    | "forall" -> Fa
+    | "exists" -> Ex
+    | _ -> failwith "not a quantifier"
+
+  let to_string = function Fa -> "forall" | Ex -> "exists"
+  let pretty_layout = function Fa -> "∀ " | Ex -> "∃ "
+end
+
 module Smtty = struct
   type t = Bool | Int | Dt [@@deriving sexp]
 
