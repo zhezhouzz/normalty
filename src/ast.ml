ec9714d7425bceb09d8ac3aa25ae65571308fb98
zhouzhezz <zorichzorich@gmail.com>
2022-09-23 18:24:06 -0400

    update

--- src/ast.ml
+++ src/ast.ml
@@ -1,106 +1,74 @@
-module Q = struct
-  type t = Fa | Ex
-
-  let is_forall = function Fa -> true | Ex -> false
-  let is_exists x = not @@ is_forall x
-
-  let of_string = function
-    | "forall" -> Fa
-    | "exists" -> Ex
-    | _ -> failwith "not a quantifier"
-
-  let to_string = function Fa -> "forall" | Ex -> "exists"
-  let pretty_layout = function Fa -> "∀ " | Ex -> "∃ "
-end
-
-module Smtty = struct
-  type t = Bool | Int | Dt [@@deriving sexp]
-
-  let smtty_eq = function
-    | Bool, Bool | Int, Int -> true
-    | Dt, Dt -> true
-    | _ -> false
-
-  let eq a b = smtty_eq (a, b)
-  let layout = function Bool -> "B" | Int -> "I" | Dt -> "D"
-
-  let pretty_typed_layout str = function
-    | Bool -> Printf.sprintf "(%s:𝓑 )" str
-    | Dt -> Printf.sprintf "(%s:𝓓 )" str
-    | Int -> str
-
-  let is_dt = function Dt -> true | _ -> false
-end
+module Q = Nt.Q
+module Smtty = Nt.Smtty
 
 module T = struct
-  open Sexplib.Std
+  include Frontend
+  include Nt.T
+  open Zzdatatype.Datatype
   open Sugar
 
-  type id = string [@@deriving sexp]
-
-  type t =
-    | Ty_unknown
-    | Ty_var of string
-    | Ty_unit
-    | Ty_int
-    | Ty_bool
-    | Ty_list of t
-    | Ty_tree of t
-    | Ty_arrow of t * t
-    | Ty_tuple of t list
-    | Ty_constructor of (id * t list)
-  [@@deriving sexp]
-
-  let is_basic_tp = function Ty_unit | Ty_int | Ty_bool -> true | _ -> false
-
-  let is_dt = function
-    | Ty_list _ | Ty_tree _ | Ty_constructor _ -> true
-    | _ -> false
-
-  let eq x y =
-    let rec aux (x, y) =
-      match (x, y) with
-      | Ty_unknown, Ty_unknown -> true
-      | Ty_var x, Ty_var y -> String.equal x y
-      | Ty_unit, Ty_unit -> true
-      | Ty_int, Ty_int -> true
-      | Ty_bool, Ty_bool -> true
-      | Ty_list x, Ty_list y -> aux (x, y)
-      | Ty_tree x, Ty_tree y -> aux (x, y)
-      | Ty_arrow (x, x'), Ty_arrow (y, y') -> aux (x, y) && aux (x', y')
-      | Ty_tuple xs, Ty_tuple ys ->
-          if List.length xs == List.length ys then
-            List.for_all aux @@ List.combine xs ys
-          else false
-      | Ty_constructor (id1, args1), Ty_constructor (id2, args2) ->
-          String.equal id1 id2
-          && List.length args1 == List.length args2
-          && List.for_all2 (fun a b -> aux (a, b)) args1 args2
-      | _ -> false
+  let _type_unify_ file line m t1 t2 =
+    let open T in
+    (* let () = Printf.printf "unify %s --> %s\n" (layout t1) (layout t2) in *)
+    let rec unify m (t1, t2) =
+      match (t1, t2) with
+      | Ty_unknown, _ -> (m, t2)
+      | _, Ty_unknown -> (m, t2)
+      | Ty_var n, t2 -> (
+          match StrMap.find_opt m n with
+          | Some t1 ->
+              let t = _check_equality file line eq t1 t2 in
+              (m, t)
+          | None ->
+              let m = StrMap.add n t2 m in
+              (m, t2))
+      | Ty_list t1, Ty_list t2 ->
+          let m, t = unify m (t1, t2) in
+          (m, Ty_list t)
+      | Ty_constructor (id1, ts1), Ty_constructor (id2, ts2) ->
+          let id = _check_equality file line String.equal id1 id2 in
+          (* let () = *)
+          (*   Printf.printf "(%s) v.s. (%s)\n" *)
+          (*     (List.split_by_comma layout ts1) *)
+          (*     (List.split_by_comma layout ts2) *)
+          (* in *)
+          let m, ts =
+            List.fold_left
+              (fun (m, ts) (t1, t2) ->
+                let m, t = unify m (t1, t2) in
+                (m, ts @ [ t ]))
+              (m, []) (List.combine ts1 ts2)
+          in
+          (m, Ty_constructor (id, ts))
+      | Ty_tree _, _ ->
+          _failatwith file line "built-in tree type, should not happen"
+      | Ty_arrow (t11, t12), Ty_arrow (t21, t22) ->
+          let m, t1 = unify m (t11, t21) in
+          let m, t2 = unify m (t12, t22) in
+          (m, Ty_arrow (t1, t2))
+      | Ty_tuple ts1, Ty_tuple ts2 when List.length ts1 == List.length ts2 ->
+          let m, ts =
+            List.fold_left
+              (fun (m, ts) (t1, t2) ->
+                let m, t = unify m (t1, t2) in
+                (m, ts @ [ t ]))
+              (m, []) (List.combine ts1 ts2)
+          in
+          (m, Ty_tuple ts)
+      | _, Ty_var _ ->
+          (m, t1)
+          (* _failatwith file line "argment should not contain type var" *)
+      | _, _ -> (
+          ( m,
+            try _check_equality file line eq t1 t2
+            with e ->
+              Printf.printf "%s != %s\n" (layout t1) (layout t2);
+              raise e ))
     in
-    aux (x, y)
+    unify m (t1, t2)
 
-  let destruct_arrow_tp tp =
-    let rec aux = function
-      | Ty_arrow (t1, t2) ->
-          let argsty, bodyty = aux t2 in
-          (t1 :: argsty, bodyty)
-      | ty -> ([], ty)
-    in
-    aux tp
-
-  let rec construct_arrow_tp = function
-    | [], retty -> retty
-    | h :: t, retty -> Ty_arrow (h, construct_arrow_tp (t, retty))
-
-  let to_smtty t =
-    let aux = function
-      | Ty_bool -> Smtty.Bool
-      | Ty_int -> Smtty.Int
-      | Ty_list _ | Ty_tree _ | Ty_constructor _ -> Smtty.Dt
-      | _ -> _failatwith __FILE__ __LINE__ "not a basic type"
-    in
-    aux t
+  let _type_unify file line t1 t2 =
+    snd @@ _type_unify_ file line StrMap.empty t1 t2
 end
 
 module NotatedT = struct
---
+++ src/nt.ml
@@ -0,0 +1,104 @@
+module Q = struct
+  type t = Fa | Ex
+
+  let is_forall = function Fa -> true | Ex -> false
+  let is_exists x = not @@ is_forall x
+
+  let of_string = function
+    | "forall" -> Fa
+    | "exists" -> Ex
+    | _ -> failwith "not a quantifier"
+
+  let to_string = function Fa -> "forall" | Ex -> "exists"
+  let pretty_layout = function Fa -> "∀ " | Ex -> "∃ "
+end
+
+module Smtty = struct
+  type t = Bool | Int | Dt [@@deriving sexp]
+
+  let smtty_eq = function
+    | Bool, Bool | Int, Int -> true
+    | Dt, Dt -> true
+    | _ -> false
+
+  let eq a b = smtty_eq (a, b)
+  let layout = function Bool -> "B" | Int -> "I" | Dt -> "D"
+
+  let pretty_typed_layout str = function
+    | Bool -> Printf.sprintf "(%s:𝓑 )" str
+    | Dt -> Printf.sprintf "(%s:𝓓 )" str
+    | Int -> str
+
+  let is_dt = function Dt -> true | _ -> false
+end
+
+module T = struct
+  open Sexplib.Std
+  open Sugar
+
+  type id = string [@@deriving sexp]
+
+  type t =
+    | Ty_unknown
+    | Ty_var of string
+    | Ty_unit
+    | Ty_int
+    | Ty_bool
+    | Ty_list of t
+    | Ty_tree of t
+    | Ty_arrow of t * t
+    | Ty_tuple of t list
+    | Ty_constructor of (id * t list)
+  [@@deriving sexp]
+
+  let is_basic_tp = function Ty_unit | Ty_int | Ty_bool -> true | _ -> false
+
+  let is_dt = function
+    | Ty_list _ | Ty_tree _ | Ty_constructor _ -> true
+    | _ -> false
+
+  let eq x y =
+    let rec aux (x, y) =
+      match (x, y) with
+      | Ty_unknown, Ty_unknown -> true
+      | Ty_var x, Ty_var y -> String.equal x y
+      | Ty_unit, Ty_unit -> true
+      | Ty_int, Ty_int -> true
+      | Ty_bool, Ty_bool -> true
+      | Ty_list x, Ty_list y -> aux (x, y)
+      | Ty_tree x, Ty_tree y -> aux (x, y)
+      | Ty_arrow (x, x'), Ty_arrow (y, y') -> aux (x, y) && aux (x', y')
+      | Ty_tuple xs, Ty_tuple ys ->
+          if List.length xs == List.length ys then
+            List.for_all aux @@ List.combine xs ys
+          else false
+      | Ty_constructor (id1, args1), Ty_constructor (id2, args2) ->
+          String.equal id1 id2
+          && List.length args1 == List.length args2
+          && List.for_all2 (fun a b -> aux (a, b)) args1 args2
+      | _ -> false
+    in
+    aux (x, y)
+
+  let destruct_arrow_tp tp =
+    let rec aux = function
+      | Ty_arrow (t1, t2) ->
+          let argsty, bodyty = aux t2 in
+          (t1 :: argsty, bodyty)
+      | ty -> ([], ty)
+    in
+    aux tp
+
+  let rec construct_arrow_tp = function
+    | [], retty -> retty
+    | h :: t, retty -> Ty_arrow (h, construct_arrow_tp (t, retty))
+
+  let to_smtty t =
+    let aux = function
+      | Ty_bool -> Smtty.Bool
+      | Ty_int -> Smtty.Int
+      | Ty_list _ | Ty_tree _ | Ty_constructor _ -> Smtty.Dt
+      | _ -> _failatwith __FILE__ __LINE__ "not a basic type"
+    in
+    aux t
+end
